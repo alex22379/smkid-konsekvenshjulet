ab8c45d03396c606d5dd7907b66871539c1dd130
Alexander <38869188+alex22379@users.noreply.github.com>
2026-04-24 18:03:26 +0200

    Add consequence pool functionality and update styles for wheel component

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,10 @@
+async function fetchConsequencePool(n = 22) {
+  const res = await fetch("/data/consequences.json");
+  const data = await res.json();
+  console.log(data)
+  return data;
+}
+
 const item = document.getElementById("item");
 const wheel = document.getElementById("wheel");
 
@@ -5,14 +12,40 @@ const wheel = document.getElementById("wheel");
 
 wheel.addEventListener("click", () => spin());
 
-function spin() {
-    if (wheel.hasAttribute("data-spinning")) return;
+let consequencePool = null;
 
-    // TODO: request consequence from pool (or just pick at random?)
+async function loadWheel() {
+    consequencePool = await fetchConsequencePool();
+    const n = consequencePool.length;
+    document.documentElement.style.setProperty("--n", n);
+    wheel.innerHTML = '';
 
-    item.innerHTML = "test";
-    wheel.setAttribute("data-spinning", null)
-
-    // TODO: notify server of consequence
+    let i = 0;
+    const degIncrement = 360 / n;
+    consequencePool.forEach((consequence) => {
+      node = document.createElement('span');
+      node.style.backgroundColor = `var(--clr-wheelItem${i % 8 + 1})`;
+      node.style.transform = `translateX(-50%) rotate(${degIncrement*i}deg) `;
+      node.innerHTML = consequence;
+      wheel.appendChild(node);
+      console.log(`Loaded consequence: "${consequence}"`);
+      i++;
+    });
 }
 
+function spin() {
+  if (wheel.hasAttribute("data-spinning")) return;
+
+  const rndConsequence = consequencePool[Math.floor(Math.random() * consequencePool.length)];
+  console.log(`Random consequence: "${rndConsequence}"`);
+
+  wheel.setAttribute("data-spinning", null);
+
+  item.innerHTML = rndConsequence;
+
+  loadWheel()
+}
+
+// init
+loadWheel()
+// /init
